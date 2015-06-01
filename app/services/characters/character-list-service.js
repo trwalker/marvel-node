6a26540068bdcab135e362abb63319fa8f642930
Tim Walker <trwalker@godaddy.com>
2015-06-01 07:54:18 -0700

    Adding character list service

---
+++ app/services/characters/character-list-service.js
@@ -0,0 +1,24 @@
+
+function CharacterListService() {
+  this.characterMapRepository_ = require('../../repositories/characters/character-map-repository');
+}
+
+function lookupCharacterList() {
+  var characterMap = this.characterMapRepository_.getCharacterMap();
+
+  var characterList = { characters: [] };
+
+  for(var key of characterMap.keys()) {
+    characterList.characters.push(key);
+  }
+
+  return characterList;
+}
+
+CharacterListService.prototype = {
+  lookupCharacterList: lookupCharacterList
+};
+
+var characterListService = new CharacterListService();
+
+module.exports = characterListService;
